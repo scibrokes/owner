e9330e1d1b9771aae01680cc73b16d57d142cb8f
englianhu <englianhu@gmail.com>
2016-01-10 14:36:35 -0500

    update

--- function/libs.R
+++ function/libs.R
@@ -9,10 +9,12 @@ if(suppressMessages(!require('BBmisc'))){
 suppressMessages(library('BBmisc'))
 
 ## Loading multiple packages at once
-pkgs <- c('plyr','dplyr','devtools','zoo','lubridate','stringr','rvest','googleVis','knitr','markdown','rmarkdown')
+pkgs <- c('plyr', 'dplyr', 'devtools', 'zoo', 'lubridate', 'stringr', 'rvest', 
+          'googleVis', 'markdown', 'rmarkdown')
 suppressAll(lib(pkgs)); rm(pkgs)
 
-# Set the googleVis options first to change the behaviour of plot.gvis, so that only the chart component of the HTML file is written into the output file.
+## Set the googleVis options first to change the behaviour of plot.gvis, so that 
+##  only the chart component of the HTML file is written into the output file.
 op <- options(gvis.plot.tag='chart')
 
 
