edd21ae414b9c087945d428321402abdb1a4289e
englianhu <englianhu@gmail.com>
2016-05-14 08:39:56 -0400

    updated education background

--- function/libs.R
+++ function/libs.R
@@ -10,7 +10,7 @@ suppressMessages(library('BBmisc'))
 
 ## Loading multiple packages at once
 pkgs <- c('plyr', 'dplyr', 'devtools', 'zoo', 'lubridate', 'stringr', 'rvest', 
-          'googleVis', 'markdown', 'knitr', 'rmarkdown')
+          'googleVis', 'markdown', 'knitr', 'rmarkdown', 'formattable')
 suppressAll(lib(pkgs)); rm(pkgs)
 
 ## Set the googleVis options first to change the behaviour of plot.gvis, so that 
