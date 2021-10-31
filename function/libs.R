57c3dda7a7559457091c2d8dfc149122ae807df8
®γσ ξηg <englianhu@gmail.com>
2021-10-31 11:55:14 +0900

    update

--- function/libs.R
+++ function/libs.R
@@ -1,14 +1,14 @@
 ## Setup Options, Loading Required Libraries and Preparing Environment
 
 ## Setting to omit all warnings
-options(warn=-1)
+options(warn = -1)
 
 ## Loading the package 'BBmisc'
 #if(suppressMessages(!require('BBmisc'))){
 #  install.packages('BBmisc')
 #  suppressMessages(library('BBmisc'))
 #}
-if(suppressMessages(!require('rmsfuns'))){
+if (suppressMessages(!require('rmsfuns'))) {
   install.packages('rmsfuns')
   suppressMessages(library('rmsfuns'))
 }
@@ -16,13 +16,13 @@ if(suppressMessages(!require('rmsfuns'))){
 ## Loading multiple packages at once
 pkgs <- c('plyr', 'dplyr', 'magrittr', 'tidyverse', 'devtools', 'zoo', 
           'lubridate', 'stringr', 'rvest', 'googleVis', 'markdown', 
-          'knitr', 'rmarkdown', 'htmltools', 'kable', 'kableExtra', 
-          'formattable', 'recharts', 'radarchart')
+          'knitr', 'rmarkdown', 'htmltools', 'knitr', 'kableExtra', 
+          'formattable', 'echarts4r', 'radarchart', 'MASS')
 #suppressAll(lib(pkgs)); rm(pkgs)
 load_pkg(pkgs); rm(pkgs)
 
 ## Set the googleVis options first to change the behaviour of plot.gvis, so that 
 ##  only the chart component of the HTML file is written into the output file.
-op <- options(gvis.plot.tag='chart')
+op <- options(gvis.plot.tag = 'chart')
 
 
