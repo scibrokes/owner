3f3e0eb6862abd9e6b4af29e136bf5260dddd785
®γσ, ξηg Lιαη Ημ（雷欧） <englianhu@gmail.com>
2023-01-03 15:50:01 +0800

    δ「Tiktok」故郷 ふるさと
    🚩🦔量化对冲，中科红旗；一带一路，泛亚高铁。
    
    黄联富本人中国福建省永春人
    
    🚩🦔《大秦赋 - 🇬🇧♰👱🏻‍♀️关雎》
    🚩🦔巫师治国，禍殃全球；
    🚩🦔巫裔尽弃，瓦釜雷鸣。
    🚩🦔商鞅变法，铲除巫裔；
    🚩🦔车裂刘瑾，中华执政。
    🚩🦔一带一路，横跨七洲；
    🚩🦔史无前例，一统天下。
    🚩🦔高频量化，对冲基金；
    🚩🦔只争朝夕，不负韶华。
    🚩🦔学海无涯，唯勤是岸；
    🚩🦔不忘初心，方得始终。
    
    **大秦赋 (Chinese Emperor)**<br>
    春秋战国《*礼记•经解*》<br>
    孔子曰：『君子慎始，差若毫厘，缪以千里。』
    
    > <span style='color:#FFEBCD; background-color:#D2B48C;'>**《礼记·经解》孔子曰：**</span><span style='color:#A9A9A9'; background-color:#696969;'>*「君子慎始。差若毫厘，谬以千里。」*</span>[^1]
    
    *引用：[「快懂百科」《礼记•经解》](https://www.baike.com/wikiid/2225522569881832051?view_id=2tt3iw3blkq000)和[第一范文网：差之毫厘，谬以千里的故事](https://www.diyifanwen.com/chengyu/liuziyishangchengyugushi/2010051523105152347092749890.htm)和[「百度百科」春秋时期孔子作品《礼记•经解》](https://baike.baidu.com/item/%E7%A4%BC%E8%AE%B0%C2%B7%E7%BB%8F%E8%A7%A3/2523092)和[「當代中國」差之毫釐 謬以千里](https://www.ourchinastory.com/zh/2962/%E5%B7%AE%E4%B9%8B%E6%AF%AB%E9%87%90%20%E8%AC%AC%E4%BB%A5%E5%8D%83%E9%87%8C)*
    
    [^1]: [HTML Color Codes](https://html-color.codes)

---
+++ 红旗礼逆袭 - 网络管理员文件设置.R
@@ -0,0 +1,6 @@
+## 出处：https://github.com/scibrokes/setup-centOS7-DO/issues/6
+
+sudo chmod 777 -R /etc/resolv.conf
+wget https://raw.githubusercontent.com/scibrokes/owner/%E4%B8%96%E5%8D%9A%E9%87%8F%E5%8C%96%E7%A0%94%E7%A9%B6%E9%99%A2/etc/resolv.conf
+sudo cp resolv.conf /etc/resolv.conf
+sudo rm resolv.conf
